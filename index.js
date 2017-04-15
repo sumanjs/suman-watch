68f1791ef71ea77496ffa768835f263450ef6969
Olegzandr <alex@oresoftware.com>
2017-04-14 23:56:38 -0700

    watch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 //core
 const util = require('util');
+const path = require('path');
 
 //npm
 const async = require('async');
@@ -13,31 +14,59 @@ const chokidar = require('chokidar');
 let io = new Server(6975, {});
 let transpile = require('./lib/transpile');
 let execute = require('./lib/execute');
+let transpileAll = require('./lib/transpile-all');
+
 
 ////////////////////////////////////////////////////////
 
 module.exports = function (obj, cb) {
+
+  const onSIG = function (){
+     console.log(' => [suman-watch] suman watch is exiting.');
+     process.exit(139);
+  };
+
+  process.on('SIGINT', onSIG);
+  process.on('SIGTERM', onSIG);
 
   const projectRoot = obj.projectRoot || su.findProjectRoot(process.cwd());
   const testSrcDir = process.env.TEST_SRC_DIR;
 
   async.autoInject({
 
-    // getSrcPaths: function(cb){
-    //   su.findSumanMarkers(['@run.sh', '@transform.sh'], testSrcDir, [], cb);
-    // },
+    getSrcPaths: function (cb) {
+      su.findSumanMarkers(['@run.sh', '@transform.sh'], testSrcDir, [], cb);
+    },
 
-    startWatching: function (cb) {
+    transpileAll: function (getSrcPaths, cb) {
+
+      const paths = Object.keys(getSrcPaths).filter(function (key) {
+        return getSrcPaths[key]['@transform.sh'];
+      })
+      .map(function(k){
+         return path.resolve(k + '/@transform.sh');
+      });
+
+      console.log('paths => ', paths);
+
+
+      transpileAll(paths, cb);
+
+    },
+
+    startWatching: function (transpileAll, cb) {
 
       let watcher = chokidar.watch(testSrcDir, {
-        ignored: /\/@target\//,
+        ignored: /\/.*target\//,
         persistent: true,
         initial: false
       });
 
       watcher.once('error', cb);
-      watcher.once('ready', function(){
-        cb(null, watcher.getWatched());
+      watcher.once('ready', function () {
+        cb(null, {
+          watched: watcher.getWatched()
+        });
       });
 
       watcher.on('change', function (f) {
@@ -63,8 +92,11 @@ module.exports = function (obj, cb) {
       throw err;
     }
 
-    console.log('=> Watching files => ', results);
+    console.log(' => Transpilation results:');
+    results.transpileAll.forEach(function(t){
+      console.log(t);
+    });
 
-  })
+  });
 
 };
