63811e6e85bb71ab03049b10f5e08b296042c2b6
Olegzandr VD <alex@oresoftware.com>
2017-12-18 22:48:03 -0800

    pdev:set

--- lib/make-execute.ts
+++ lib/make-execute.ts
@@ -2,6 +2,7 @@
 
 //dts
 import {INearestRunAndTransformRet} from "suman-types/dts/suman-utils";
+import {IAtConfig, IEnv} from "suman-types/dts/at-config";
 import {ChildProcess} from "child_process";
 
 //polyfills
@@ -32,28 +33,45 @@ process.stderr.setMaxListeners(80);
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 export const makeExecute = function (watchOptions: Object, projectRoot: string) {
-
+  
   return function (f: string, runData: INearestRunAndTransformRet, $cb: Function) {
-
+    
     const cb = su.once(this, $cb);
-    let runPath: string;
-
+    let runPath: string, config: IAtConfig, env: IEnv;
+    
     // we do a lazy check to see if the @config file is closer to the source file, by simply comparing
     // filepath length
     const runLength = runData.run ? runData.run.length : 0;
     if (runData.config && runData.config.length > runLength) {
       try {
-        const config = require(runData.config);
-        const plugin = config['@run']['plugin']['value'];
-        if (plugin) {
-          runPath = require(plugin).getRunPath();
+        config = require(runData.config);
+        env = config['@run'] && config['@run']['env'];
+        const pluginValue = config['@run']['plugin']['value'];
+        if (pluginValue) {
+          runPath = require(pluginValue).getRunPath();
         }
       }
       catch (err) {
         log.warning('no run plugin could be found to execute the test.');
       }
     }
-
+    
+    if(env){
+      assert(su.isObject(env), 'env must be a plain object.');
+    }
+    
+    let getEnv = function(){
+      let basic = env || {};
+      return Object.assign({}, process.env, basic, {
+        SUMAN_PROJECT_ROOT: projectRoot,
+        SUMAN_CHILD_TEST_PATH: f,
+        SUMAN_TEST_PATHS: JSON.stringify([f]),
+        SUMAN_ALWAYS_INHERIT_STDIO: 'yes',
+        SUMAN_WATCH_TEST_RUN: 'yes'
+      });
+    };
+    
+    
     if (!runPath) {
       if (runData.run) {
         log.warning('runPath has been found.');
@@ -63,25 +81,31 @@ export const makeExecute = function (watchOptions: Object, projectRoot: string) 
         log.warning('runPath has not been found.');
       }
     }
-
+    
     let handleStdioAndExit = function (k: ChildProcess, runPath: string, f: string) {
-
+  
+      k.once('error', function (e: Error) {
+        log.error(e.stack || e);
+      });
+      
       let stdout = '';
       k.stdout.setEncoding('utf8');
       k.stdout.pipe(pt(chalk.grey(' [watch-worker-exec] '))).pipe(process.stdout);
       k.stdout.on('data', function (d: string) {
         stdout += d;
       });
-
+      
       let stderr = '';
       k.stderr.setEncoding('utf8');
       k.stderr.pipe(pt(chalk.yellow.bold(' [watch-worker-exec] '), {omitWhitespace: true})).pipe(process.stderr);
       k.stderr.on('data', function (d: string) {
         stderr += d;
       });
-
+      
       k.once('exit', function (code: number) {
-
+        
+        k.removeAllListeners();
+        
         cb(null, {
           path: f,
           runPath,
@@ -89,54 +113,42 @@ export const makeExecute = function (watchOptions: Object, projectRoot: string) 
           stdout: String(stdout).trim(),
           stderr: String(stderr).trim()
         });
-
+        
       });
     };
-
+    
     let k;
-
+    
     if (runPath) {
-
+      
       log.info(`executable path => '${runPath}'`);
-
+      
       k = cp.spawn('bash', [], {
         detached: false,
         cwd: projectRoot,
         stdio: ['pipe', 'pipe', 'pipe'],
-        env: Object.assign({}, process.env, {
-          SUMAN_PROJECT_ROOT: projectRoot,
-          SUMAN_TEST_PATHS: JSON.stringify([f]),
-          SUMAN_ALWAYS_INHERIT_STDIO: 'yes',
-          SUMAN_CHILD_TEST_PATH: f,
-          SUMAN_WATCH_TEST_RUN: 'yes'
-        })
+        env: getEnv()
       });
-
+      
       fs.createReadStream(runPath).pipe(k.stdin);
-
       handleStdioAndExit(k, runPath, null);
-
+      
     }
     else if (String(f).endsWith('.js')) {
-
+      
       let noDaemon = function () {
-
-        k = cp.spawn('node', [f], {
+        
+        k = cp.spawn('bash', [], {
           detached: false,
           cwd: projectRoot,
-          stdio: ['ignore', 'pipe', 'pipe'],
-          env: Object.assign({}, process.env, {
-            SUMAN_PROJECT_ROOT: projectRoot,
-            SUMAN_CHILD_TEST_PATH: f,
-            SUMAN_TEST_PATHS: JSON.stringify([f]),
-            SUMAN_ALWAYS_INHERIT_STDIO: 'yes',
-            SUMAN_WATCH_TEST_RUN: 'yes'
-          })
+          stdio: ['pipe', 'pipe', 'pipe'],
+          env: getEnv()
         });
-
+        
+        k.stdin.end(`\nnode ${f};\n`);
         handleStdioAndExit(k, runPath, f);
       };
-
+      
       // if we are using Suman daemon, then we can use this?
       const client = net.createConnection({port: 9091}, () => {
         log.good('connected to server!');
@@ -144,7 +156,7 @@ export const makeExecute = function (watchOptions: Object, projectRoot: string) 
         log.good('cwd-dir => ', cwdDir);
         client.write(JSON.stringify({pid: -1, cwd: cwdDir, args: [f]}) + '\r\n');
       });
-
+      
       client.once('error', function (e) {
         if (/ECONNREFUSED/.test(e.message)) {
           process.nextTick(noDaemon);
@@ -154,14 +166,14 @@ export const makeExecute = function (watchOptions: Object, projectRoot: string) 
           process.nextTick(cb, e);
         }
       });
-
+      
       let endOk = false;
       client.once('data', function () {
         endOk = true;
       });
-
+      
       client.pipe(pt(` [watch-worker-via-daemon] `)).pipe(process.stdout);
-
+      
       client.once('end', () => {
         if (endOk) {
           cb(null, {
@@ -173,41 +185,30 @@ export const makeExecute = function (watchOptions: Object, projectRoot: string) 
           });
         }
       });
-
+      
     }
     else {
-
+      
       log.info(`file path  => '${f}'`);
-
+      
       let extname = path.extname(f);
-
+      
       if (['.html', '.json', '.xml', '.log', '.txt', '.d.ts', '.md'].some(v => String(f).endsWith(v))) {
         log.info(`files with extension '${extname}' are currently ignored by suman-watch.`);
         return process.nextTick(cb, null, {code: -1});
       }
-
+      
       k = cp.spawn('bash', [], {
         detached: false,
         cwd: projectRoot,
         stdio: ['pipe', 'pipe', 'pipe'],
-        env: Object.assign({}, process.env, {
-          SUMAN_PROJECT_ROOT: projectRoot,
-          SUMAN_CHILD_TEST_PATH: f,
-          SUMAN_TEST_PATHS: JSON.stringify([f]),
-          SUMAN_ALWAYS_INHERIT_STDIO: 'yes',
-          SUMAN_WATCH_TEST_RUN: 'yes'
-        })
+        env: getEnv()
       });
       
       k.stdin.end(`\nexec ${f};\n`);
-
-      k.once('error', function (e: Error) {
-        log.error(e.stack || e);
-      });
-
       handleStdioAndExit(k, runPath, f);
     }
-
+    
   }
-
+  
 };
